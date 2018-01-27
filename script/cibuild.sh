92babd7a39369734b6310e2c55132f44460b85d5
fraz3alpha <andrewtaylor2@gmail.com>
2018-01-27 20:17:58 +0000

    Switch from Travis deploy method to our custom one
      - It effectively does the same thing, or would, if Travis was working

--- script/cibuild.sh
+++ script/cibuild.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# This script pushing the built copy of the this site to a staging repository
+
 # Enable exit on failure
 set -e
 
@@ -17,7 +19,17 @@ bundle exec jekyll build
 echo "Built site, total size: `du -sh ${SITE_DIR}`"
 
 # Initialise the git repo
-#cd ${SITE_DIR}
-#git init
+cd ${SITE_DIR}
+# Add a file to say that the site doesn't need building
+touch .nojekyll
+
+# Setup git to push to the staging repo
+git init
 # Add the target remote
-#git remote add origin
+git remote add staging https://${NERDY_DAY_TRIPS_GITHUB_TOKEN}@github.com/fraz3alpha/nerdy-day-trips.git
+# Create a new branch, and commit all the code
+git checkout -b gh-pages
+git add -A
+git commit -m 'Travis build for production'
+git log -1
+git push --force staging gh-pages
